d3da62bdc1e9b9408dd9a51e148234ba5c1bc1b4
sonnt612 <39478081+sonnt612@users.noreply.github.com>
2023-03-27 11:43:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { Dimensions, Platform, StatusBar } from "react-native";
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 const STATUSBAR_DEFAULT_HEIGHT = 20;
 const STATUSBAR_X_HEIGHT = 44;
 const STATUSBAR_IP12_HEIGHT = 47;
 const STATUSBAR_IP12MAX_HEIGHT = 47;
-const STATUSBAR_IP14PRO_HEIGHT = 49;
+const STATUSBAR_IP14PRO_HEIGHT = 54;
 
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
@@ -24,7 +24,7 @@ const IP14PRO_HEIGHT = 852;
 const IP14PROMAX_WIDTH = 430;
 const IP14PROMAX_HEIGHT = 932;
 
-const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get("window");
+const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
 let statusBarHeight = STATUSBAR_DEFAULT_HEIGHT;
 let isIPhoneX_v = false;
@@ -34,36 +34,38 @@ let isIPhone12Max_v = false;
 let isIPhoneWithMonobrow_v = false;
 let isIPhoneWithDynamicIsland_v = false;
 
-if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
-  if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhoneX_v = true;
-    statusBarHeight = STATUSBAR_X_HEIGHT;
-  } else if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhoneXMax_v = true;
-    statusBarHeight = STATUSBAR_X_HEIGHT;
-  } else if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhone12_v = true;
-    statusBarHeight = STATUSBAR_IP12_HEIGHT;
-  } else if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhone12Max_v = true;
-    statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
-  } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
-    isIPhoneWithDynamicIsland_v = true;
-    statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
-  } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
-    isIPhoneWithDynamicIsland_v = true;
-    statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
-  }
+if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
+    if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneX_v = true;
+        statusBarHeight = STATUSBAR_X_HEIGHT;
+    } else if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneXMax_v = true;
+        statusBarHeight = STATUSBAR_X_HEIGHT;
+    } else if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone12_v = true;
+        statusBarHeight = STATUSBAR_IP12_HEIGHT;
+    } else if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone12Max_v = true;
+        statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
+    } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneWithDynamicIsland_v = true;
+        statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
+    } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneWithDynamicIsland_v = true;
+        statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
+    }
 }
 
-export const isIPhoneX = () => isIPhoneX_v;
-export const isIPhoneXMax = () => isIPhoneXMax_v;
-export const isIPhone12 = () => isIPhone12_v;
-export const isIPhone12Max = () => isIPhone12Max_v;
+export const isIPhoneX = () =>  isIPhoneX_v;
+export const isIPhoneXMax = () =>  isIPhoneXMax_v;
+export const isIPhone12 = () =>  isIPhone12_v;
+export const isIPhone12Max = () =>  isIPhone12Max_v;
 export const isIPhoneWithMonobrow = () => isIPhoneWithMonobrow_v;
 export const isIPhoneWithDynamicIsland = () => isIPhoneWithDynamicIsland_v;
 
@@ -72,9 +74,9 @@ const getExpoRoot = () => global.Expo || global.__expo || global.__exponent;
 export const isExpo = () => getExpoRoot() !== undefined;
 
 export function getStatusBarHeight(skipAndroid) {
-  return Platform.select({
-    ios: statusBarHeight,
-    android: skipAndroid ? 0 : StatusBar.currentHeight,
-    default: 0,
-  });
+    return Platform.select({
+        ios: statusBarHeight,
+        android: skipAndroid ? 0 : StatusBar.currentHeight,
+        default: 0
+    })
 }
